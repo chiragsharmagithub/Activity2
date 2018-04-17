2d9639b962efe7c424e8df988e5c1d4026b64333
Chirag <chiragsharmaindian@gmail.com>
2018-04-18 00:08:13 +0530

    Initial Commit

---
+++ app/src/main/java/appclub/chirag/appticationclub/appclub/Event.java
@@ -0,0 +1,15 @@
+package appclub.chirag.appticationclub.appclub;
+
+/**
+ * Created by Rohaan on 12-Dec-17.
+ */
+
+public class Event {
+
+    public final String title;
+
+
+    public Event(String title) {
+        this.title = title;
+    }
+}
